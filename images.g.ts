c9f10304fec468b79de69de328566dc796978e96
Bruh-v <153546951+Bruh-v@users.noreply.github.com>
2023-12-11 22:20:41 +0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Finn":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 1 . . . 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . . 1 d d d 1 . . . . . . 
+. . . . . 1 f d f 1 . . . . . . 
+. . . . . 1 d d d 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . . 9 9 9 9 9 . . . . . . 
+. . . . 9 7 9 9 9 7 9 . . . . . 
+. . . . d 7 9 9 9 7 d . . . . . 
+. . . . d 9 9 9 9 9 d . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . . f f . f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
